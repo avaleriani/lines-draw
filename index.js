dedd34e98de3925f07bf2dd85f011b0173cb4e85
Agustin <avaleriani@users.noreply.github.com>
2016-04-12 19:00:41 -0300

    removed scrollmagic

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict';
 var $ = require("../jquery");
 var shortid = require('../shortid');
-var scrollMagic = require('../scrollmagic');
 var point = require('./lib/point');
 var line = require('./lib/line');
 var linesDrawer = {
