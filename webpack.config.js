23ea7cc077762f3c020f0b53b0ca159ba7ec4cd0
Agustin <avaleriani@users.noreply.github.com>
2016-04-14 23:57:48 -0300

    several updates and reordering

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var Path = require('path');
+var webpack = require('webpack');
+module.exports = {
+    cache: true,
+    debug: true,
+    devtool: 'eval',
+    entry: './lib/main.js',
+    output: {
+        path: Path.join(__dirname, "dist"),
+        filename: 'build.js'
+    },
+    resolve: {
+        extensions: ['', '.js', '.json'],
+        root: [Path.join(__dirname, "node_modules")],
+        modulesDirectories: ['node_modules'],
+        alias: {
+            "ScrollMagic": Path.resolve('node_modules', 'scrollmagic/scrollmagic/uncompressed/ScrollMagic.js'),
+            "animation.velocity": Path.resolve('node_modules', 'scrollmagic/scrollmagic/uncompressed/plugins/animation.velocity.js'),
+            "velocity": Path.resolve('node_modules', 'velocity-animate/velocity.min.js'),
+            "debug.addIndicators" :Path.resolve('node_modules', 'scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators.js')
+        }
+    }
+};
